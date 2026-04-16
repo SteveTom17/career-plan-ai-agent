8829de5bcc2b4297d2a9a365533843b8ba4284cd
SteveTom17 <3248788195@qq.com>
2026-04-16 23:40:05 +0800

    RAG基础

---
+++ src/main/java/com/itheima/aiagent/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.itheima.aiagent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String health(){
+        return "health";
+    }
+}
